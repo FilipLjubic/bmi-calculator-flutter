95fc9c57cd3e4da48468ddec138af2d39f8756ba
Filip Ljubic <f.ljubic7@hotmail.com>
2020-07-31 21:46:59 +0200

    base ui for real

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -14,64 +14,39 @@ class _InputPageState extends State<InputPage> {
         centerTitle: true,
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    margin: EdgeInsets.all(15.0),
+                  child: MyContainer(
+                    color: Color(0xFF1D1E33),
                   ),
                 ),
                 Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    margin: EdgeInsets.all(15.0),
+                  child: MyContainer(
+                    color: Color(0xFF1D1E33),
                   ),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              margin: EdgeInsets.all(15.0),
+            child: MyContainer(
+              color: Color(0xFF1D1E33),
             ),
           ),
           Expanded(
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    margin: EdgeInsets.all(15.0),
+                  child: MyContainer(
+                    color: Color(0xFF1D1E33),
                   ),
                 ),
                 Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    margin: EdgeInsets.all(15.0),
+                  child: MyContainer(
+                    color: Color(0xFF1D1E33),
                   ),
                 ),
               ],
@@ -79,6 +54,23 @@ class _InputPageState extends State<InputPage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class MyContainer extends StatelessWidget {
+  final Color color;
+
+  MyContainer({@required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.all(15.0),
     );
   }
 }
